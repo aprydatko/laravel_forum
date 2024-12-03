8132aa5a37d3aeb1c956022df2b05eb1c57b4cd4
Artur Prydatko <artyrpridatko@gmail.com>
2024-12-03 20:27:48 +0200

    add children branch

---
+++ app/Http/Resources/Branch/BranchWithChildrenResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\Branch;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BranchWithChildrenResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'section_id' => $this->section_id,
+            'parent_id' => $this->parent_id,
+            'children' => BranchResource::collection($this->children)->resolve(),
+        ];
+    }
+}
