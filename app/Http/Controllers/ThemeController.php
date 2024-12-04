32a310e71646151a860c5d0462082b56072dd72b
Artur Prydatko <artyrpridatko@gmail.com>
2024-12-04 21:34:10 +0200

    add edit and delete theme

--- app/Http/Controllers/ThemeController.php
+++ app/Http/Controllers/ThemeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\Theme\StoreRequest;
 use App\Http\Requests\Theme\UpdateRequest;
+use App\Http\Resources\Theme\ThemeResource;
 use App\Models\Theme;
 use Illuminate\Http\Request;
 
@@ -62,7 +63,8 @@ class ThemeController extends Controller
      */
     public function edit(Theme $theme)
     {
-        //
+        $theme = ThemeResource::make($theme)->resolve();
+        return inertia('Theme/Edit', compact('theme'));
     }
 
     /**
@@ -74,7 +76,10 @@ class ThemeController extends Controller
      */
     public function update(UpdateRequest $request, Theme $theme)
     {
-        //
+        $data = $request->validated();
+        $theme->update($data);
+
+        return redirect()->route('branches.show', $theme->branch_id);
     }
 
     /**
@@ -85,6 +90,7 @@ class ThemeController extends Controller
      */
     public function destroy(Theme $theme)
     {
-        //
+        $theme->delete();
+        return redirect()->route('branches.show', $theme->branch_id);
     }
 }
---
+++ resources/js/Pages/Theme/Edit.vue
@@ -0,0 +1,53 @@
+
+<template>
+    <div>
+        <div class="flex items-center mb-4">
+            <h3 class="text-xl mr-4">Редактировать Тему</h3>
+        </div>
+        <div>
+            <div class="mb-4">
+                <input type="text" placeholder="Заголовок" v-model="title" class="border-gray-300 p-2 w-1/2" />
+            </div>
+            <div class="mb-4">
+                <textarea placeholder="Описание" v-model="description" class="border-gray-300 p-2 w-1/2">
+                    {{ description }}
+                </textarea>
+            </div>
+            <div>
+                <a
+                    class="block w-1/2 py-2 bg-white border border-sky-600 text-black text-center"
+                    @click.prevent="update" href="">Сохранить</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import MainLayout from "@/Layouts/MainLayout.vue";
+    import {Link} from "@inertiajs/vue3";
+
+    export default {
+        name: 'Create',
+        layout: MainLayout,
+        props: ['theme'],
+        components: {
+            Link
+        },
+        data() {
+            return {
+                title: this.theme.title,
+                description: this.theme.description,
+                branch_id: this.branch_id,
+            }
+        },
+        methods: {
+            update() {
+                this.$inertia.patch(`/themes/${this.theme.id}`, {
+                    title: this.title,
+                    description: this.description,
+                });
+            }
+        }
+    }
+</script>
+
